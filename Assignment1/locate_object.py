8b293bc8b882d383a009adefc17a93647af0413d
walle <asfadsfasdf@gasdfasdfads.com>
2017-05-28 16:46:13 -0400

    change locate_object

--- Assignment1/locate_object.py
+++ Assignment1/locate_object.py
@@ -3,10 +3,11 @@ import time
 
 INCR = 10
 SAMPLE = 10
-REQUIRED = 8
+REQUIRED = 7
 
 def scanOneEighty():
         objectDetected = False
+        angle = 0
         
         for i in range(0, 180, INCR):
                 sampling = []
@@ -14,7 +15,7 @@ def scanOneEighty():
                 time.sleep(0.3)
                 print("angle ", i)
                 
-                for i in range(SAMPLE):
+                for j in range(SAMPLE):
                         time.sleep(0.05)
                         d = us_dist(15)
                         
@@ -25,36 +26,69 @@ def scanOneEighty():
 
                 if len(sampling) >= REQUIRED:
                         objectDetected = True
+                        angle = i
                         break
                 
-        return objectDetected
+        return objectDetected, angle
                 
 
 if __name__ == "__main__":
-        objectDected = False
+        objectDetected = False
+        angle = 0
         servo(90)
  	time.sleep(1)
 
- 	set_speed(60)
+ 	set_speed(50)
  	time.sleep(1)
  	
- 	
-        objectDetected = scanOneEighty()
-
-        if not objectDetected:
-                set_speed(100)
-                
+ 	while not objectDetected:
                 start = time.time()
                 left_rot()
 
                 while True:
-                        if time.time() - start >= 5:
+                        if time.time() - start >= 2:
                                 stop()
                                 break
+                        
+                objectDetected, angle = scanOneEighty()
+        
+        servo(90)
+        set_speed(50)
+        
+        t = (angle - 90) / 20        
+        rotateLeft = True
+        
+        if time < 0:
+                rotateLeft = False
 
-                objectDetected = scanOneEighty()
+        t = abs(t)
 
-                
+        start = time.time()
+        
+        if rotateLeft:
+                left_rot()
+        else:
+                right_rot()
+
+        """ need to consider the sonic conic angle to adjust the robot to face
+           the front of the detected object """
+        while True:
+                if time.time() - start >= t:
+                        stop()
+                        break
+
+        d = us_dist(15)
+
+        if d < 25:
+                motor_bwd()
+                while d < 20:
+                        d = us_dist(15)
+                stop()
+        else:
+                motor_fwd()
+                while d > 25:
+                        d = us_dist(15)
+                stop()
 
         
 
