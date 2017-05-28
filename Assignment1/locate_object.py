837f91589de2acb562eb7aaf2c22a6d05d0b8a31
walle <asfadsfasdf@gasdfasdfads.com>
2017-05-28 13:50:40 -0400

    locate_object modified

--- Assignment1/locate_object.py
+++ Assignment1/locate_object.py
@@ -1,20 +1,67 @@
 from gopigo import *
 import time
 
+INCR = 10
+SAMPLE = 10
+REQUIRED = 8
+
+def scanOneEighty():
+        objectDetected = False
+        
+        for i in range(0, 180, INCR):
+                sampling = []
+                servo(i)
+                time.sleep(0.3)
+                print("angle ", i)
+                
+                for i in range(SAMPLE):
+                        time.sleep(0.05)
+                        d = us_dist(15)
+                        
+                        print(d)
+                        
+                        if d <= 100 and d > 0:
+                                sampling.append(d)
+
+                if len(sampling) >= REQUIRED:
+                        objectDetected = True
+                        break
+                
+        return objectDetected
+                
+
 if __name__ == "__main__":
-	servo(90)
+        objectDected = False
+        servo(90)
  	time.sleep(1)
 
  	set_speed(60)
  	time.sleep(1)
+ 	
+ 	
+        objectDetected = scanOneEighty()
 
- 	right_rot()
+        if not objectDetected:
+                set_speed(100)
+                
+                start = time.time()
+                left_rot()
 
-	while True:
-		print us_dist(15)
+                while True:
+                        if time.time() - start >= 5:
+                                stop()
+                                break
 
-		if us_dist(15) <= 100:
-			stop()
-			break
+                objectDetected = scanOneEighty()
 
+                
+
+        
+
+                
+                        
+                
+                
+        
+			
 
