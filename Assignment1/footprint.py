62b0acf08c241e1c7ac0194a470b29640a9e58a4
walle <asfadsfasdf@gasdfasdfads.com>
2017-05-29 23:19:26 -0400

    change footprint

--- Assignment1/footprint.py
+++ Assignment1/footprint.py
@@ -1,7 +1,7 @@
 from gopigo import *
 import time
 import numpy as np
-from scipy.optimize import fsolve
+#from scipy.optimize import fsolve
 
 SAMPLE = 10
 REQUIRED = 8
@@ -9,7 +9,17 @@ REQUIRED = 8
 #Test 1
 Width1 = 31.5
 Distance1 = 43.5
-Angle1 = atan(32.5/2,43.5)*2.0
+#Angle1 = atan(32.5/2,43.5)*2.0
+
+#def calculate_angle(alpha, d):
+#    m = 0
+#    e = 0
+#    l = d*np.tan(alpha/2)
+#
+#    return [m+e-l, -np.sin(np.arctan(m/d)) - ((e**2+m**2-l**2)/(2*e*np.sqrt(m**2+d**2)))]
+
+def calculate_angle(alpha, d):
+    theta = 
 
 if __name__ == "__main__":
     angles = []
@@ -43,18 +53,17 @@ if __name__ == "__main__":
     calculate theta as the final result
     """
     alpha = angles[-1] - angles[0]
+    print("alpha",angles[-1] - angles[0])
     d = us_dist(15)
-    m, e = fsolve(calculate_angle(), [alpha, d])
+    m, e = calculate_angle(alpha, d)
+    print("m",m)
+    print("e",e)
     theta0 = np.arctan(m/d)
-    theta = (alpha - theta0*2)  
+    theta = (alpha - theta0*2)
+    print("theta",theta)
     #########################
 
-    print(angles[-1] - angles[0])
+    
 
         
-def calculate_angle(alpha, d):
-    m = 0
-    e = 0
-    l = d*tan(alpha/2)
 
-    return [m+e-l, -np.sin(np.arctan(m/d)) - ((e**2+m**2-l**2)/(2*e*np.sqrt(m**2+d**2)))]
--- Assignment1/test.py
+++ Assignment1/test.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
    	time.sleep(1)
         dis = us_dist(15)
 
+        set_speed(100)
  	left_rot()
 
  	while True:
             i = us_dist(15)
-
+            time.sleep(0.2)
             print(i)
