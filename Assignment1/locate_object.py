ef5360b62802eb8e103bb5348edac2b4b326fb25
walle <asfadsfasdf@gasdfasdfads.com>
2017-05-30 01:07:55 -0400

    change locate_object

--- Assignment1/locate_object.py
+++ Assignment1/locate_object.py
@@ -5,69 +5,30 @@ SAMPLE = 10
 REQUIRED = 7
 STOP_DIST = 20
 
-if __name__ == "__main__":
-    objectDetected = False
-    angle = 0
-    servo(0)
-    time.sleep(1)
+def left_deg(deg=None):
+    '''
+    Turn chassis left by a specified number of degrees.
+    DPR is the #deg/pulse (Deg:Pulse ratio)
+    This function sets the encoder to the correct number
+     of pulses and then invokes left().
+    '''
+    if deg is not None:
+        pulse= int(deg/DPR)
+        enc_tgt(0,1,pulse)
+    left()
 
-    inc = 3
-    for ang in range(0,180,inc):
-        sampling = []
-        servo(ang)
-        time.sleep(0.3)
-        print("angle ", i)
-        
-        for j in range(SAMPLE):
-                time.sleep(0.05)
-                d = us_dist(15)
-                
-                print(d)
-                
-                if d <= 100 and d > 0:
-                        sampling.append(d)
+def right_deg(deg=None):
+    '''
+    Turn chassis right by a specified number of degrees.
+    DPR is the #deg/pulse (Deg:Pulse ratio)
+    This function sets the encoder to the correct number
+     of pulses and then invokes right().
+    '''
+    if deg is not None:
+        pulse= int(deg/DPR)
+        enc_tgt(1,0,pulse)
+    right()
 
-        if len(sampling) >= REQUIRED:
-                objectDetected = True
-                angle = i
-                break   
-        if objectDetected:             
-            objectDetected, angle = scanOneEighty()
-            break
-        left_deg(inc)
-    # if no object detected within 180 degrees
-    if objectDetected == False:
-        turn_to(180)
-        servo(0)
-        for ang in range(0,180,inc):
-            sampling = []
-            servo(ang)
-            time.sleep(0.3)
-            print("angle ", i)
-            
-            for j in range(SAMPLE):
-                    time.sleep(0.05)
-                    d = us_dist(15)
-                    
-                    print(d)
-                    
-                    if d <= 100 and d > 0:
-                            sampling.append(d)
-
-            if len(sampling) >= REQUIRED:
-                    objectDetected = True
-                    angle = i
-                    break   
-            if objectDetected:             
-                objectDetected, angle = scanOneEighty()
-                break
-            left_deg(inc)
-
-    servo(90)
-    #set_speed(50)
-    turn_to(angle)
-    move(STOP_DIST)                                
-	
 def move(min_dist):
     ## Set servo to point straight ahead
     servo(90)
@@ -101,3 +62,72 @@ def turn_to(angle):
         right_deg(-1 * degs)
     ## This sleep is really just for debugging so I can verify that it turned properly
     time.sleep(1)
+
+    
+
+
+if __name__ == "__main__":
+    objectDetected = False
+    angle = 0
+    servo(0)
+    DPR = 360.0/64
+
+    inc = 4
+    for ang in range(1,179,inc):
+        sampling = []
+        time.sleep(0.05)
+        #print("angle ", i)
+        
+        for j in range(SAMPLE):
+                time.sleep(0.01)
+                d = us_dist(15)
+                
+                #print(d)
+                
+                if d <= 100 and d > 0:
+                        sampling.append(d)
+
+        if len(sampling) >= REQUIRED:
+                objectDetected = True
+                angle = i
+                break   
+        if objectDetected:
+            angle = ang
+            #objectDetected, angle = scanOneEighty()
+            break
+        servo(ang)
+        #left_deg(inc)
+    # if no object detected within 180 degrees
+    if objectDetected == False:
+        left_deg(180)
+        servo(0)
+        for ang in range(1,179,inc):
+            sampling = []
+            time.sleep(0.05)
+            print("angle ", i)
+            
+            for j in range(SAMPLE):
+                    time.sleep(0.05)
+                    d = us_dist(15)
+                    
+                    print(d)
+                    
+                    if d <= 100 and d > 0:
+                            sampling.append(d)
+
+            if len(sampling) >= REQUIRED:
+                    objectDetected = True
+                    angle = i
+                    break   
+            if objectDetected:
+                angle = ang
+                #objectDetected, angle = scanOneEighty()
+                break
+            servo(ang)
+            
+
+    servo(90)
+    #set_speed(50)
+    turn_to(angle)
+    move(STOP_DIST)                                
+
