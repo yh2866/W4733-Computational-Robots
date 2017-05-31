fcc59394b2e6195a1f05edf268d8840c9a9c35d4
Yuanqing Hong <yh2866@columbia.edu>
2017-05-31 14:59:35 -0400

    Update dance.py

--- Assignment1/dance.py
+++ Assignment1/dance.py
@@ -4,15 +4,15 @@ import time
 
 
 if __name__ == "__main__":
+    #Dance options
     move_options = { 0 : motor_fwd,
                      1 : motor_bwd,
                      2 : left,
                      3 : left_rot,
                      4 : right,
                      5 : right_rot,
-
     }
-
+    #Speed options
     speed_options = {0 : 30,
                      1 : 60,
                      2 : 90,
@@ -27,14 +27,14 @@ if __name__ == "__main__":
 
     while True:
         end = time.time()
-
+        #Set the total running time is 20
         if end - start > 20:
             break
-
-
+        
         moveStart = time.time()
+        #
         i = random.randint(0, 7)
-
+        
         while True:
             set_speed(speed_options[i])
             moveEnd = time.time()
@@ -50,10 +50,3 @@ if __name__ == "__main__":
         print("inner while ended")
 
     stop()
-
-
-
-
-
-
-
