c7246c48e1a1186601b80e583b53f2035cbd3159
walle <asfadsfasdf@gasdfasdfads.com>
2017-05-31 15:10:10 -0400

    footprint.py changed

--- Assignment1/footprint.py
+++ Assignment1/footprint.py
@@ -1,18 +1,10 @@
 from gopigo import *
 import time
 import numpy as np
-#from scipy.optimize import fsolve
 
 SAMPLE = 10
 REQUIRED = 8
 
-
-#def calculate_angle(alpha, d):
-#    m = 0
-#    e = 0
-#    l = d*np.tan(alpha/2)
-#
-#    return [m+e-l, -np.sin(np.arctan(m/d)) - ((e**2+m**2-l**2)/(2*e*np.sqrt(m**2+d**2)))]
 
 def calculate_angle(alpha, d):
    m = 31.5/2
@@ -20,43 +12,80 @@ def calculate_angle(alpha, d):
    print ("object_angle",2.*theta)
    return alpha - 2.*theta
 
+def methodOne():
+   angles = []
+
+   servo(90)
+   time.sleep(1)
+
+   set_speed(60)
+   time.sleep(1)
+
+   mini_dis= 100
+    
+   for i in range(20, 160, 2):
+      sampling = []
+      servo(i)
+      time.sleep(0.3)
+      print("angle ", i)
+        
+      for j in range(SAMPLE):
+         time.sleep(0.01)
+         d = us_dist(15)
+                
+         print(d)
+                
+         if d <= 100 and d > 0:
+            sampling.append(d)
+
+         if len(sampling) >= REQUIRED:
+            angles.append(i)
+
+   #########################
+   """
+   calculate theta as the final result
+   """
+   alpha = angles[-1] - angles[0]
+   print ("alpha",angles[-1] - angles[0])
+   mini_dis = 30
+   print ("result",calculate_angle(alpha, mini_dis))
+
+#####Detect the object#######
+def detect(dist):
+    SAMPLE = 5
+    REQUIRED = 3
+    
+    sampling = []
+    for j in range(SAMPLE):
+        time.sleep(0.07)
+        
+        d = us_dist(15)
+        print(d)
+        if d <= dist and d > 0:
+            sampling.append(d)
+
+    if len(sampling) >= REQUIRED:
+        return True
+    else:
+        return False
+
+
+def methodTwo():
+   servo(90)
+   time.sleep(0.1)
+
+   angles = []
+   dist = 100
+
+   for j in range(90, 180, 2):
+      servo(j)
+      if detect(dist):
+         angles.append(j)
+   print("start ", angles[0])
+   print("end ", angles[-1])
+   return (angles[-1] - angles[0]) * 2
+
 
 if __name__ == "__main__":
-    angles = []
-
-    servo(90)
-    time.sleep(1)
-
-    set_speed(60)
-    time.sleep(1)
-
-    mini_dis= 100
-    
-    for i in range(20, 160, 2):
-        sampling = []
-        servo(i)
-        time.sleep(0.3)
-        print("angle ", i)
-        
-        for j in range(SAMPLE):
-                time.sleep(0.01)
-                d = us_dist(15)
-                
-                print(d)
-                
-                if d <= 100 and d > 0:
-                        sampling.append(d)
-
-        if len(sampling) >= REQUIRED:
-                angles.append(i)
-
-    #########################
-    """
-    calculate theta as the final result
-    """
-    alpha = angles[-1] - angles[0]
-    print ("alpha",angles[-1] - angles[0])
-    #d = us_dist(15)
-    print ("mini_dis",mini_dis)
-    mini_dis = 30
-    print ("result",calculate_angle(alpha, mini_dis))
+    # methodOne()
+    print(methodTwo())
