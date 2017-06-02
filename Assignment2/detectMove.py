66ff63e075e7010dcd5b412aedd4603c3c00282d
walle <asfadsfasdf@gasdfasdfads.com>
2017-06-02 14:44:47 -0400

    small changes

---
+++ Assignment2/detectMove.py
@@ -0,0 +1,4 @@
+from gopigo import *
+import time
+
+if __name__ == '__main__':
---
+++ Assignment2/move.py
@@ -0,0 +1,34 @@
+from gopigo import *
+import time
+
+DPR = 360.0/64
+
+def left_deg(deg=None):
+    '''
+    Turn chassis left by a specified number of degrees.
+    DPR is the #deg/pulse (Deg:Pulse ratio)
+    This function sets the encoder to the correct number
+     of pulses and then invokes left().
+    '''
+    if deg is not None:
+        pulse= int(deg/DPR)
+        enc_tgt(0,1,pulse)
+    left()
+
+
+def right_deg(deg=None):
+    '''
+    Turn chassis right by a specified number of degrees.
+    DPR is the #deg/pulse (Deg:Pulse ratio)
+    This function sets the encoder to the correct number
+     of pulses and then invokes right().
+    '''
+    if deg is not None:
+        pulse= int(deg/DPR)
+        enc_tgt(1,0,pulse)
+    right()
+
+
+if __name__ == '__main__':
+    set_speed(1000)
+    left_deg(90)
