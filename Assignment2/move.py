908394eace613429b8124f6491bc030ee2f32b1e
Yuanqing Hong <yh2866@columbia.edu>
2017-06-02 14:53:15 -0400

    Update move.py

--- Assignment2/move.py
+++ Assignment2/move.py
@@ -40,6 +40,22 @@ def fwd_cm(dist=None):
         enc_tgt(1,1,pulse)
     fwd()
 
+
+def cm2pulse(dist):
+    '''
+    Calculate the number of pulses to move the chassis dist cm.
+    pulses = dist * [pulses/revolution]/[dist/revolution]
+    '''
+    wheel_circ = 2*math.pi*WHEEL_RAD # [cm/rev] cm traveled per revolution of wheel
+    revs = dist/wheel_circ
+    PPR = 18 # [p/rev] encoder Pulses Per wheel Revolution
+    pulses = PPR*revs # [p] encoder pulses required to move dist cm.
+    if en_debug:
+        print 'WHEEL_RAD',WHEEL_RAD
+        print 'revs',revs
+        print 'pulses',pulses
+    return pulses
+    
 if __name__ == '__main__':
     set_speed(1000)
     fwd_cm(10)
