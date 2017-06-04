9e8604dba2a686209de4d59185711450d9cc4866
myf123asd <yuanfeng2016@hotmail.com>
2017-06-04 16:27:58 -0400

    Update On_mline.py

--- Assignment2/On_mline.py
+++ Assignment2/On_mline.py
@@ -1,7 +1,18 @@
+def distance(x,y,k):
+    return abs(y-k*x)/np.sqrt(1+k**2)
+
 def On_mline(x_goal, y_goal, x, y):
     #y=kx
+    error = 0.2
+    if(x_goal==0): #k not exist
+        if(x<=error):
+            return True
+        else:
+            return False
+
     k = y_goal/x_goal
-    if(y==k*x):
+    dist = distance(x,y,k)
+    if(dist<=error):
         return True
     else:
         return False
