6fbce42044514407218fbc851a81ec26e93d6855
myf123asd <yuanfeng2016@hotmail.com>
2017-06-16 22:34:57 -0400

    Update Graham Algorithm.py

--- Assignment3/Graham Algorithm.py
+++ Assignment3/Graham Algorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def find_angle(x, y):
     """
@@ -12,10 +13,13 @@ def sort_points(points_array):
     up = 300
     rightmost = points_array[0]
     for i in range(len(points_array)):
-        if(points_array[i][1]<up):  #below the curr
-            if(rightmost[0]<points_array[0]):   #to the right of curr
+        if(points_array[i][1]==up):
+            if(rightmost[0]<points_array[i][0]):
                 rightmost = points_array[i]
-                up = rightmost[1]
+        if(points_array[i][1]<up):  #below the curr
+            #if(rightmost[0]<points_array[i][0]):   #to the right of curr
+            rightmost = points_array[i]
+            up = rightmost[1]
     print "rightmost:", points_array[0]
 
     #store point with angle
@@ -65,13 +69,17 @@ def graham_scan(points_array):
     #print "tttte",points_array[1:-1]
     for point in points_array[1:]:
         #print point
+        while len(s)>1 and toTheLeft(point, s[-1], s[-2])==False:
+            s.pop()
+        s.append(point)
+        """
         if(toTheLeft(point, s[-1], s[-2])):
             s.append(point)
             print s,point
         else:
             s.pop()
             print s,point
-
+        """
     #extract points from stack
     result = []
     for i in range(len(s)):
@@ -81,6 +89,24 @@ def graham_scan(points_array):
     return result[::-1] #reverse list
 
 
-points_array = [[1,0],[1,1],[0.5,0],[0,1],[0.5,0.5]]
+#points_array = [[1,0],[1,1],[0.5,0],[0,1],[0.5,0.5]]
+
+points_array = [[200.0, 220.0], [221.21320343599999, 241.21320343599999], [210.60660171800001, 251.819805153], [189.39339828199999, 230.60660171800001], [177.0, 197.0], [198.21320343599999, 218.21320343599999], [187.60660171800001, 228.819805153], [166.39339828199999, 207.60660171800001], [177.0, 220.0], [198.21320343599999, 241.21320343599999], [187.60660171800001, 251.819805153], [166.39339828199999, 230.60660171800001], [200.0, 197.0], [221.21320343599999, 218.21320343599999], [210.60660171800001, 228.819805153], [189.39339828199999, 207.60660171800001]]
+"""
+points_array = ([[130, 180],
+                    [159.997181494, 179.588779362],
+                    [145.35287801, 194.640170509]])
+"""
+for i in range(len(points_array)):
+    plt.plot(points_array[i][0], points_array[i][1], 'bo')
+
 result = graham_scan(points_array)
+print(len(result))
+for i in range(len(result)):
+    plt.plot(result[i][0], result[i][1], 'ro')
+
+plt.xlim([0,300])
+plt.ylim([0,300])
+
+plt.show()
 print result
